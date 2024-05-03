9f6a77f3377812076a82214603de8e7be4906d36
Alexandre Direito <xandedsj@gmail.com>
2024-05-03 11:33:24 -0300

    Desafio de criar um gerador de senhas

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+Random random = new Random();
+
+int rdn = random.Next(2);
+int tamanhoSenha = (rdn == 0) ? 16 : 14;
+string caixaCaracteres = "ABCDEFGHIJKLMNOPQRSTUVXYZ<>:?!@#$%&*()_+{}";
+StringBuilder senha = new StringBuilder();
+
+for (int cont = 0; cont < tamanhoSenha; cont++)
+{
+    int indiceCaixa = random.Next(0, caixaCaracteres.Length - 1);
+    senha.Append(caixaCaracteres[indiceCaixa]);
+}
+
+Console.WriteLine("Senha gerada:");
+Console.WriteLine(senha.ToString());
